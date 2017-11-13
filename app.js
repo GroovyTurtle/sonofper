d7d380124fbe19c7a328c138edde3add570e5369
Freddy Persson <freddy.persson@glimworks.com>
2017-11-13 14:08:51 +0100

    Stuff

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('./www'));
 
-app.listen(80, () => console.log('Listening on port 80!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));
