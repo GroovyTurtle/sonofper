a0ad116b5b0a60a756cf1eb822dd86ce40772f3a
Freddy Persson <FreddyPersson85@gmail.com>
2018-04-17 05:14:48 +0000

    Changed port fort the basic site

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static(__dirname + '/www', {extensions: ['html']}));
 
-app.listen(3001, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log('Listening on port 3000!'));
